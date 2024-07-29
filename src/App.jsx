5d65717c711947877f4be0824f1af8b275c2b6b3
preetikumari15 <annukumari0928@gmail.com>
2024-07-29 17:02:14 +0530

    Add Initial Files

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import { useState, useEffect } from 'react'
+import {TodoProvider} from './context'
+import './App.css'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
+
+function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev] )
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))
+
+    
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    //console.log(id);
+    setTodos((prev) => 
+    prev.map((prevTodo) => 
+      prevTodo.id === id ? { ...prevTodo, 
+        completed: !prevTodo.completed } : prevTodo))
+  }
+
+  useEffect(() => {
+    try {
+      // Retrieve todos from localStorage
+      const todosString = localStorage.getItem("todos");
+
+      // Check if todosString is not null
+      if (todosString) {
+        // Parse the JSON string into an array
+        const todos = JSON.parse(todosString);
+
+        // Check if the parsed value is an array
+        if (Array.isArray(todos)) {
+          setTodos(todos);
+        }
+      }
+    } catch (error) {
+      // Handle any JSON parsing errors
+      console.error("Failed to parse todos from localStorage:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+  
+
+
+
+  return (
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                        <TodoForm />
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                          <div key={todo.id}
+                          className='w-full'
+                          >
+                            <TodoItem todo={todo} />
+                          </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+    </TodoProvider>
+  )
+}
+
+export default App
